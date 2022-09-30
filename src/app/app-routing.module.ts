d59c1257b066d80957f880cf73735981c87d30ab
sagarchavan07 <chavansagar96044@gmail.com>
2022-09-30 22:51:05 +0530

    used SCSS in dashboard , addContact & getAllContacts API

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormComponent } from './form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'form', component: FormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AddressbookService } from '../service/addressbook.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  contacts: any = [];
 
-  constructor() { }
+  constructor(private router: Router, private service: AddressbookService) { }
 
   ngOnInit(): void {
+    this.getAllContacts();
+  }
+
+  openForm() {
+    this.router.navigate(['form']);
+  }
+
+  getAllContacts() {
+    this.service.getAllContacts().subscribe((response) => {
+      this.contacts = response.data;
+    });
+  }
+
+  deleteContact(contactId: number) {
+    this.service.deleteContactById(contactId).subscribe((Response) => {
+      console.log("contact deleted " + Response.data);
+      alert("contact deleted of id: " + contactId);
+      this.getAllContacts();
+    })
   }
 
 }
